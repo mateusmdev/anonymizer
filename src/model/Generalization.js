cd3a4d122e64623e757aeff6befa3b7a8be9e221
Mateus Developer <mateusm.developer@gmail.com>
2024-05-30 14:18:55 -0300

    ok

--- src/model/Generalization.js
+++ src/model/Generalization.js
@@ -8,27 +8,28 @@ class Generalization extends IAnonymizerStrategy{
   }
 
   async execute(data){
-    console.log(console.log(data))
     const result = {}
-    console.log(result)
-
-    const promiseArray = data.map(field => {
-      return new Promise(async function(resolve, reject){
-        const { value, type, anon } = field
+  
+    const promiseArray = Object.entries(data).map(field => {
+      return new Promise(async (resolve, reject) => {
+        const [key, data] = field
+        const { value, type, anon } = data
 
         if (type === 'name'){
-          result[field] = value
+          result[key] = anon
           return resolve()
         }
-
-        const query = this._getGeneralizationSQL(type, anon)
+        
+        const query = this._getGeneralizationSQL(type, value, anon)
         if (query == null) throw new Error('InvalidType Error.')
 
-        const anonymizedData = await sequelize.query(query, {
-          type: QueryTypes.SELECT,
+        const [anonymizedData] = await sequelize.query(query, {
+          type: QueryTypes.SELECT
         })
 
-        result[field] = anonymizedData
+        const sanitizedResult = this._sanitizeResult(anonymizedData)
+
+        result[key] = sanitizedResult
         resolve()
       })
     })
@@ -41,13 +42,19 @@ class Generalization extends IAnonymizerStrategy{
   }
 
   _getGeneralizationSQL(dataType, value, anon){
+    console.log(dataType)
     const methods = {
       zipcode: `SELECT anon.generalize_int4range(${value})`,
-      daterange: `SELECT anon.generalize_daterange(${value}, '${anon}')`
+      daterange: `SELECT anon.generalize_daterange('${value}', '${anon}')`
     }
 
     const sql = methods[dataType] || null
     return sql
+  }
+
+  _sanitizeResult(anonymizedData){
+    const values = Object.values(anonymizedData)[0].map(obj => obj.value);
+    return values
   }
 }
 

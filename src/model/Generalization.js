babfd86932d7a2c5202470ffa9ead961782c0364
Mateus Developer <mateusm.developer@gmail.com>
2024-05-29 22:10:59 -0300

    implementação do algoritimo

--- src/model/Generalization.js
+++ src/model/Generalization.js
@@ -1,5 +1,6 @@
 const IAnonymizerStrategy = require('./../interface/IAnonymizerStrategy')
 const sequelize = require('./../database/sequelize')
+const { QueryTypes } = require('sequelize')
 
 class Generalization extends IAnonymizerStrategy{
   constructor(){
@@ -8,18 +9,45 @@ class Generalization extends IAnonymizerStrategy{
 
   async execute(data){
     console.log(console.log(data))
-    const promiseResult = data.map(field => {
-      return new Promise(function(resolve, reject){
+    const result = {}
+    console.log(result)
+
+    const promiseArray = data.map(field => {
+      return new Promise(async function(resolve, reject){
         const { value, type, anon } = field
 
-        const callback = this._getGeneralizationMethod(type)
+        if (type === 'name'){
+          result[field] = value
+          return resolve()
+        }
+
+        const query = this._getGeneralizationSQL(type, anon)
+        if (query == null) throw new Error('InvalidType Error.')
+
+        const anonymizedData = await sequelize.query(query, {
+          type: QueryTypes.SELECT,
+        })
+
+        result[field] = anonymizedData
+        resolve()
       })
     })
-    return 
+
+    console.log(promiseArray)
+    await Promise.all(promiseArray)
+
+    console.log(result)
+    return result
   }
 
-  _getGeneralizationMethod(dataType, anonValue){
+  _getGeneralizationSQL(dataType, value, anon){
+    const methods = {
+      zipcode: `SELECT anon.generalize_int4range(${value})`,
+      daterange: `SELECT anon.generalize_daterange(${value}, '${anon}')`
+    }
 
+    const sql = methods[dataType] || null
+    return sql
   }
 }
 
